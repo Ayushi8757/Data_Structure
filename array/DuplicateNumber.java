6df3cda5f3f66abf49816ed37546f3b2dd203233
Kumari Ayushi <ayushisomya1611@gmail.com>
2024-11-06 20:52:22 +0530

    adding some line of code

--- array/DuplicateNumber.java
+++ array/DuplicateNumber.java
@@ -1,13 +1,18 @@
+
+import java.util.Arrays;
+
 public class DuplicateNumber {
     public static void main(String[] args) {
-        int arr[] = { 1, 4, 3, 5, 4, 3 };
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    System.out.println(arr[i]);
-                }
-            }
+        int arr[] = { 1, 4, 3, 5, 4, 3, 4 };
+        Arrays.sort(arr);
 
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] == arr[i - 1]) {
+                continue;
+            } else if (arr[i] == arr[i + 1]) {
+                System.out.println(arr[i]);
+            }
         }
     }
 }
